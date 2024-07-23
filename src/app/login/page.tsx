e5e26c822e4df65073c7cd349a3675539a6b4404
zhangrui10 <zhangrui10@joyy.com>
2024-07-23 15:00:28 +0800

    feat: 登入页面增加已登入自动跳转

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,45 +1,14 @@
-'use client'
-import React from 'react'
-import {Button, Form, Input, Space, Typography} from 'react-vant'
-import styles from './page.module.css'
-import Link from 'next/link'
-import {login} from '@/actions/common/login'
-import {toastResult} from '@/util/toastResult'
+import {LoginForm} from '@/components/login/LoginForm'
+import {HOME_PAGE} from '@/constant'
+import {cookieJwtCheck} from '@/util/cookieJwtCheck'
+import {redirect} from 'next/navigation'
 
-const Login = () => {
-  return (
-    <div className={styles.login}>
-      <div className={styles.title}>
-        <Typography.Title level={2}>登入</Typography.Title>
-      </div>
-      <Form
-        title="登入"
-        footer={
-          <Space className={styles.buttons} direction="vertical">
-            <Button block type="primary" nativeType="submit">
-              确定
-            </Button>
-            <Link href="/register">
-              <Button block>注册</Button>
-            </Link>
-          </Space>
-        }
-        onFinish={async values => {
-          const result = await login(values)
-          if (result) {
-            toastResult(result)
-          }
-        }}
-      >
-        <Form.Item label="用户名" name="username">
-          <Input placeholder="请输入用户名" />
-        </Form.Item>
-        <Form.Item label="密码" name="password">
-          <Input placeholder="请输入密码" type="password" />
-        </Form.Item>
-      </Form>
-    </div>
-  )
+const Login = async () => {
+  if (await cookieJwtCheck()) {
+    redirect(HOME_PAGE)
+  } else {
+    return <LoginForm />
+  }
 }
 
 export default Login
