0cd5b7fc716e907a7579de795a10d3138b8f142f
zhangrui10 <zhangrui10@joyy.com>
2024-07-23 14:51:24 +0800

    feat: 首页跳转

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,27 @@
-/** @type {import('next').NextConfig} */
-
 import withImages from 'next-images'
 import withTM from 'next-transpile-modules'
 import withPlugins from 'next-compose-plugins'
 
-const nextConfig = withPlugins([
-  withTM(['react-vant']),
-  withImages({
-    webpack(config, options) {
-      return config
+/** @type {import('next').NextConfig} */
+const nextConfig = withPlugins(
+  [
+    withTM(['react-vant']),
+    withImages({
+      webpack(config, options) {
+        return config
+      },
+    }),
+  ],
+  {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/bookkeeping',
+          permanent: true,
+        },
+      ]
     },
-  }),
-])
+  },
+)
 export default nextConfig
