5d67a7ec91763215c47b4941fbcc40088c624b70
zhangrui10 <zhangrui10@joyy.com>
2024-07-24 16:40:57 +0800

    feat: CustomerPicker支持取消

--- src/components/customer/CustomerPicker/index.tsx
+++ src/components/customer/CustomerPicker/index.tsx
@@ -2,10 +2,12 @@ import {FullScreenPopup} from '@/components/ui/FullScreenPopup'
 import {Typography} from 'react-vant'
 import {CustomerList} from '../CustomerList'
 import {Customer} from '@prisma/client'
+import {Cross} from '@react-vant/icons'
+import Styles from './index.module.css'
 
 interface CustomerPickerProps {
   value?: Customer
-  onChange?: (value: Customer) => void
+  onChange?: (value: Customer | undefined) => void
   placeholder?: string
 }
 export const CustomerPicker = (props: CustomerPickerProps) => {
@@ -13,13 +15,26 @@ export const CustomerPicker = (props: CustomerPickerProps) => {
 
   return (
     <>
-      <a href="#customer" style={{width: '100%', display: 'inline-block'}}>
-        {value?.name === undefined ? (
-          <Typography.Text type="secondary">{placeholder}</Typography.Text>
-        ) : (
-          <Typography.Text>{value?.name}</Typography.Text>
-        )}
-      </a>
+      <div className={Styles.flexWrap}>
+        <a href="#customer" className={Styles.link}>
+          {value?.name === undefined ? (
+            <Typography.Text type="secondary">{placeholder}</Typography.Text>
+          ) : (
+            <Typography.Text>{value?.name}</Typography.Text>
+          )}
+        </a>
+        {value ? (
+          <a
+            onClick={e => {
+              e.preventDefault()
+              onChange?.(undefined)
+            }}
+            className={Styles.cancel}
+          >
+            <Cross style={{verticalAlign: 'center'}} />
+          </a>
+        ) : null}
+      </div>
       <FullScreenPopup position="right" triggerHash="#customer">
         <CustomerList
           selectable
