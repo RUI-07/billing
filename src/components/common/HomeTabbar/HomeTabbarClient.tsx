24ea256c294e28d44c904449c64d71c6f87c7608
zhangrui10 <zhangrui10@joyy.com>
2024-07-29 21:10:42 +0800

    feat: HomeTabbar增加prefetch

--- src/components/common/HomeTabbar/HomeTabbarClient.tsx
+++ src/components/common/HomeTabbar/HomeTabbarClient.tsx
@@ -1,12 +1,25 @@
 'use client'
 import {CashOnDeliver, ChartTrendingO, FriendsO, Records, UserCircleO} from '@react-vant/icons'
 import {usePathname, useRouter} from 'next/navigation'
+import {useEffect} from 'react'
 import {Tabbar, TabbarProps} from 'react-vant'
+
+const items = [
+  {name: 'bookkeeping', icon: <Records />, title: '记账'},
+  {name: 'ledger', icon: <CashOnDeliver />, title: '总帐本'},
+  {name: 'customer', icon: <FriendsO />, title: '客户'},
+  {name: 'statistics', icon: <ChartTrendingO />, title: '查询统计'},
+  {name: 'my', icon: <UserCircleO />, title: '我的'},
+]
 
 export const HomeTabbarClient = (props: TabbarProps) => {
   const router = useRouter()
   const pathname = usePathname()
   const firstPath = pathname.split('/').slice(1)[0]
+
+  useEffect(() => {
+    items.forEach(item => router.prefetch('/' + item.name))
+  }, [router])
 
   return (
     <Tabbar
@@ -16,21 +29,13 @@ export const HomeTabbarClient = (props: TabbarProps) => {
         v && router.push('/' + v)
       }}
     >
-      <Tabbar.Item name="bookkeeping" icon={<Records />}>
-        记账
-      </Tabbar.Item>
-      <Tabbar.Item name="ledger" icon={<CashOnDeliver />}>
-        总帐本
-      </Tabbar.Item>
-      <Tabbar.Item name="customer" icon={<FriendsO />}>
-        客户
-      </Tabbar.Item>
-      <Tabbar.Item name="statistics" icon={<ChartTrendingO />}>
-        查询统计
-      </Tabbar.Item>
-      <Tabbar.Item name="my" icon={<UserCircleO />}>
-        我的
-      </Tabbar.Item>
+      {items.map(item => {
+        return (
+          <Tabbar.Item key={item.name} name={item.name} icon={item.icon}>
+            {item.title}
+          </Tabbar.Item>
+        )
+      })}
     </Tabbar>
   )
 }
