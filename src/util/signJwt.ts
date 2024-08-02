6dabd61171d94e6aafd8dc1e0f42cb1778e155e3
zhangrui10 <zhangrui10@joyy.com>
2024-08-02 14:34:25 +0800

    feat: jwt过期时间改为1周

--- src/util/signJwt.ts
+++ src/util/signJwt.ts
@@ -3,7 +3,7 @@ import {SignJWT} from 'jose'
 const ISSUER = process.env.APP_NAME!
 const AUDIENCE = `${process.env.APP_NAME}-api`
 
-const EXPIRE = '1day'
+const EXPIRE = '1week'
 
 export const signJwt = async (payload: Record<string, any>) => {
   const secret = new TextEncoder().encode(process.env.SECRET)
